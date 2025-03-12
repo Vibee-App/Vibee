d45fdf8ea3b738866dbe4951c2c3d93e55fb1cb1
souhaila <razfasouhaila20@gmail.com>
2025-03-12 16:42:27 +0100

    add page conexion

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,7 +2,7 @@ import { root } from '@lynx-js/react';
 import { App } from './App.jsx';
 import { createMemoryRouter, RouterProvider } from 'react-router';
 import {Landing} from './pages/landing/Landing.tsx';
-
+import Login from './pages/Login/Login.tsx';
 import Profile from './pages/profile/Profile.jsx';
 import Events from './pages/events/Events.jsx';
 
@@ -11,6 +11,7 @@ const router = createMemoryRouter([
     path: '/',
     element: <App/>,
     children: [
+      { path: 'login', element: <Login /> },
       { path: 'landing', element: <Landing /> },
       { path: 'profiles', element: <Profile /> },
       { path: 'events', element: <Events /> },
