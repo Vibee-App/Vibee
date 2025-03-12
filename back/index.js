b291f71df0fd780923549e0df2b6599d9d5153fd
Potowai <81572547+Potowai@users.noreply.github.com>
2025-03-12 16:51:33 +0100

    fix back + port

--- back/index.js
+++ back/index.js
@@ -1,19 +1,17 @@
 // Importation des modules nécessaires
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const express = require('express');  // Importation de Express
-const process = require('process');
-const basename = path.basename(__filename);  // Nom du fichier actuel
-const env = process.env.NODE_ENV || 'development';  // Environnement (par défaut 'development')
-const config = require(__dirname + '/config/config.js')[env];  // Chargement de la config en fonction de l'environnement
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const express = require("express"); // Importation de Express
+const process = require("process");
+const basename = path.basename(__filename); // Nom du fichier actuel
+const env = process.env.NODE_ENV || "development"; // Environnement (par défaut 'development')
+const config = require(__dirname + "/config/config.js")[env]; // Chargement de la config en fonction de l'environnement
 const db = {};
 
 // Initialisation de l'application Express
 const app = express();
-const port = 4000;  // Port sur lequel le serveur écoutera
-
-
+const port = process.env.PORT || 4000; // Port sur lequel le serveur écoutera
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,14 +20,19 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
 // Définir les routes
-app.use('/api/authent', require('./routes/UserRoute.js'));
+app.use("/api/authent", require("./routes/UserRoute.js"));
 
-app.get('/ok', (req, res) => {
-  res.send('ok');
+app.get("/ok", (req, res) => {
+  res.send("ok");
 });
 
 // Démarrer le serveur
