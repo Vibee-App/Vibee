f02665bb0248d25256bef780e262b7193afdb027
Delépine Tom <tom.delepine@supdevinci-edu.fr>
2025-03-13 09:00:15 +0100

    create_accueil V2

--- front/src/pages/accueil/Accueil.tsx
+++ front/src/pages/accueil/Accueil.tsx
@@ -2,23 +2,48 @@ import { useNavigate, useEffect } from 'react-router';
 import { useState} from '@lynx-js/react';
 import './Accueil.css';
 
+interface Event {
+    event_id: number;
+    image: string;
+    event: string;
+    tags: string[];
+    heure_debut: string;
+    date_debut: string;
+    adresse: string;
+    resumer: string;
+  }
+  
+
 export function Accueil() {
-    const [listEvents, setListEvents] = useState([]);
-
-  let navigate = useNavigate();
-
+    const [listEvents, setListEvents] = useState<Event[]>([]);
+    const [loadPage, setLoad] = useState(true);
+    
   useEffect(() => {
 
   }, []);
 
-  const createObjetData = () => {
-    const event = {
-        Image : "url",
-        Nom : "name",
-
-    }
+  const createObjetsData = () => {
+    const event: Event = {
+      event_id: 7473738,
+      image: 'url',
+      event: 'name',
+      tags: ['hfzehh', 'dzjkjkkz'],
+      heure_debut: 'heure',
+      date_debut: '12 Mars 2025',
+      adresse: '6 rue du chandelier',
+      resumer: 'hzefjsdhhyjd',
+    };
+    setListEvents((prev) => [...prev, event]);
+    setLoad
   };
   
+  if(loadPage == true){
+    return (
+      <view className="input-card-url loading">
+        <text>Chargement...</text>
+      </view>
+    )
+  }
   return (
     <list
     scroll-orientation="vertical"
