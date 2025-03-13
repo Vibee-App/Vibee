25efecbaa15dab73a38a86f7fe7dad9143593337
souhaila <razfasouhaila20@gmail.com>
2025-03-13 10:37:24 +0100

    Add page inscription

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -3,6 +3,7 @@ import { App } from './App.jsx';
 import { createMemoryRouter, RouterProvider } from 'react-router';
 import {Landing} from './pages/landing/Landing.tsx';
 import Login from './pages/Login/Login.tsx';
+import Signup from './pages/Signup/Signup.tsx';
 import Profile from './pages/profile/Profile.jsx';
 import Events from './pages/events/Events.jsx';
 
@@ -11,7 +12,9 @@ const router = createMemoryRouter([
     path: '/',
     element: <App/>,
     children: [
+
       { path: 'login', element: <Login /> },
+      { path: '/signup', element: <Signup /> },
       { path: 'landing', element: <Landing /> },
       { path: 'profiles', element: <Profile /> },
       { path: 'events', element: <Events /> },
